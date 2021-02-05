c38952857a1afd0a5f1b1743c3b19c15388443a0
YJ Wang <yenjanw@users.noreply.github.com>
2021-02-05 13:45:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize("indicators_C.pyx",
+    ext_modules = cythonize("tail_C.pyx",
                             compiler_directives={'language_level' : "3"}),
     include_dirs=[numpy.get_include()]
 )
